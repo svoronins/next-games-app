40abf521c578271e24fc38bc271e40a6ddf509ff
Sergejs Voronins <sergejs.voronins@symplify.com>
2025-01-29 14:39:30 +0100

    cleaning

--- components/SignOutButton.tsx
+++ components/SignOutButton.tsx
@@ -6,9 +6,6 @@ export function SignOutButton({ action }: { action: () => void }) {
     <button
       className="w-full text-start block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
       role="menuitem"
-      // onClick={async () => {
-      //   await signOut();
-      // }}
       onClick={action}
     >
       Sign out
