0b77423770b0f3d34caadd587f032eb4e8ebc49c
Sergejs Voronins <sergejs.voronins@symplify.com>
2025-01-30 09:52:22 +0100

    vercel fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
 export default function Main({}) {
-  return <>Enviroment:{process.env.TEST_VAR}</>;
+  return (
+    <>
+      Enviroment:{process.env.TEST_VAR}
+      <br /> Database url: {process.env.DATABASE_URL}
+    </>
+  );
 }
